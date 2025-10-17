c178e0145dca607877bdcf8a5d27e7274ddf84ab
Ian Gitonga <139679065+gitonga-ik@users.noreply.github.com>
2025-10-17 23:03:47 +0300

    Corrections

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,13 +1,17 @@
 import mysql.connector
+import os
+
+host = os.environ.get("host")
+user = os.environ.get("user")
+password = os.environ.get("password")
 
 try:
-    connection = mysql.connector.connect(host="localhost", user="iann34", password="root67")
-    database = "alx_book_store"
+    connection = mysql.connector.connect(host=host, user=user, password=password)
     cursor = connection.cursor()
-    command = f"CREATE DATABASE {database}"
+    command = "CREATE DATABASE IF NOT EXISTS alx_book_store"
 
     cursor.execute(command)
-    print(f"Database '{database}' created successfully!")
+    print("Database 'alx_book_store' created successfully!")
     cursor.close()
     connection.close()
 except mysql.connector.errors.DatabaseError as error:
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from intro-to-db!")
-
-
-if __name__ == "__main__":
-    main()
